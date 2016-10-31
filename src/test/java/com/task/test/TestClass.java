13f1e9f3106e61810e17aa6649f5771e40025dfa
Andrew <grigoruk1990@mail.ru>
2016-11-01 00:09:12 +0200

    added webdrivermanager

--- src/test/java/com/task/test/TestClass.java
+++ src/test/java/com/task/test/TestClass.java
@@ -1,16 +1,21 @@
 package com.task.test;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.MarionetteDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+
 /**
  * Created by Andrii_Grygoruk on 10/26/2016.
  */
 public class TestClass {
+
     @Test
     public void startChrome(){
+        ChromeDriverManager.getInstance().setup();
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://www.google.com.ua");
         driver.close();
@@ -18,8 +23,10 @@ public class TestClass {
 
     @Test
     public void startFirefox(){
+        MarionetteDriverManager.getInstance().setup();
         WebDriver driver = new FirefoxDriver();
         driver.navigate().to("https://www.google.com.ua");
         driver.close();
+        driver.quit();
     }
 }
